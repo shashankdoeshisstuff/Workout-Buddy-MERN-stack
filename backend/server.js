e1944e21c0200c057b4b8fc958443b2b60081b1f
Shashank Maurya <moonshader0@gmail.com>
2023-11-17 22:27:47 +0530

    added mongodb link and connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -17,7 +18,14 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening to the port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connecting to db & listening to the port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
