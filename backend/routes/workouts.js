72907f08a4674c32ed39a3eb59f80fe2661ce690
Shashank Maurya <moonshader0@gmail.com>
2023-11-17 23:47:28 +0530

    added workout file and post request

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -14,8 +15,17 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST a new workout'})
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
+    // res.json({mssg: 'POST a new workout'})
 })
 
 // DELETE a workout
