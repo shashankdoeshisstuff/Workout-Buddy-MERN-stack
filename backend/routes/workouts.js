b45b3d9d281b455d7e9c7aaf2c5f4346ef128a3f
Shashank Maurya <moonshader0@gmail.com>
2023-11-21 12:07:07 +0530

    create function to update and delete the workout

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,7 +2,9 @@ const express = require('express')
 const { 
     createWorkout,
     getWorkout,
-    getWorkouts
+    getWorkouts,
+    deleteWorkout,
+    updateWorkout
  } = require('../controllers/workoutController')
 
 const router = express.Router()
@@ -18,13 +20,9 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
