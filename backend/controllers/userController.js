35c1e10f10ff02bb62d49ada1a38ff094d462d60
Shashank Maurya <moonshader0@gmail.com>
2023-11-29 22:04:56 +0530

    added token from jsonwebtoken, made a function to generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,29 @@
-const User = require('../models/userModer')
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
-// login user
-const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'})
 }
 
-// signup user
+// login a user
+const loginUser = async (req, res) => {
+  res.json({mssg: 'login user'})
+}
+
+// signup a user
 const signupUser = async (req, res) => {
-    res.json({mssg: 'signup user'})
+  const {email, password} = req.body
+
+  try {
+    const user = await User.signup(email, password)
+
+    // create token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
 module.exports = { signupUser, loginUser }
